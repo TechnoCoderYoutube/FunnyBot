709de0437cffe07f2cff09ef6717b8e67438d39e
Techno Coder <45730490+TechnoCoderYoutube@users.noreply.github.com>
2019-01-05 19:32:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  msg.guild.send('FunnyBot is Online');
 });
 
 var prefix = "!"
