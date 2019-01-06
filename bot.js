2f2b643c6a8440c915e5175a3ccfd79cc3f1839d
Techno Coder <45730490+TechnoCoderYoutube@users.noreply.github.com>
2019-01-06 15:19:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
     msg.reply('!ping,');
     msg.reply('new commands and features will come soon. Thanks for using, our website: https://www.funnybot.tk');
   }
-  if (msg.content === 'hey', 'hi', 'hello'){
+  if (msg.content === 'hey'){
   msg.reply(', Hows it going??? Welcome');
             }
 });
